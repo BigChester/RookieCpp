ee7d400d68f814c5293fa851238c7abc3369e5fd
Chester <602639269@qq.com>
2024-11-21 00:09:16 +0800

    Count a Fabonacci Array with using Vector

--- EssentialCpp/Chapter1_BacisCppProgramming/HelloWorld.cpp
+++ EssentialCpp/Chapter1_BacisCppProgramming/HelloWorld.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(void) {
+    /* Hello World */
     cout << "Hello World!\n";
+
+    /* Fabonacci Array */
+    const int seqLength = 10;
+
+    vector<int> FibonacciSeq;
+    FibonacciSeq.push_back(1);
+    FibonacciSeq.push_back(1);
+
+    cout << "Fibonacci array: "
+         << FibonacciSeq[0] << " "
+         << FibonacciSeq[1] << " ";
+
+    for (size_t i = 2; i < seqLength; i++) {
+        int next = FibonacciSeq[i-1] + FibonacciSeq[i-2];
+        FibonacciSeq.push_back(next);
+
+        cout << FibonacciSeq[i] << " ";
+    }
 
     return 0;
 }
